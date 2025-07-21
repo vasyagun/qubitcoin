3b3ff2905a5c432a4af5091575562fde78d083c9
vasyagun <63818342+vasyagun@users.noreply.github.com>
2025-07-21 11:38:57 +0300

    Update setup_docker_run.sh

--- setup_docker_run.sh
+++ setup_docker_run.sh
@@ -36,14 +36,19 @@ fi
 # Проверка NVIDIA Toolkit
 if ! docker info | grep -q "Runtimes: nvidia"; then
   echo "[*] Устанавливаю NVIDIA Container Toolkit..."
-  distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-  curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | sudo apt-key add -
-  curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list |
-    sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-  sudo apt update
-  sudo apt install -y nvidia-docker2
+
+  distribution=$(. /etc/os-release; echo $ID$VERSION_ID)
+  curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+
+  curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list \
+    | sed 's#deb #deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] #' \
+    | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+  sudo apt-get update
+  sudo apt-get install -y nvidia-docker2
   sudo systemctl restart docker
 fi
+
 
 # Запуск
 echo "[*] Запуск майнера..."
